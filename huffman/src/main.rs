f29809f9994d2d166dbc8cb283002eec679cda84
rabuu <rabuu.dev@gmail.com>
2021-10-17 16:29:51 +0200

    Remove unnecessary bin crate

--- huffman/src/main.rs
+++
@@ -1,8 +0,0 @@
-use huffman::Tree;
-
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut tree = Tree::new_from_string(String::from("lorem ipsum"));
-    tree.build();
-    println!("{:#?}", tree);
-    Ok(())
-}
